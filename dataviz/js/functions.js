e039d0b8c3a2cce2f2bdd83266591a94f77a47b8
danielflorez111 <danielflorez111@hotmail.com>
2018-08-21 23:58:46 -0500

    Scroll

---
+++ dataviz/js/functions.js
@@ -0,0 +1,35 @@
+function cleanCanvas(element) {
+    d3.select(element).html(null);
+}
+
+Reveal.addEventListener('slide1', () => {
+    console.log('Slide 1');
+    cleanCanvas('#slide1container');
+    d3.select('#slide1container')
+        .append("svg")
+        .attr("width", 500)
+        .attr("height", 500)
+        .append("circle")
+        .attr("cx", 100)
+        .attr("cy", 100)
+        .attr("r", 100)
+        .style("fill", "purple");
+});
+
+Reveal.addEventListener('slide2', () => {
+    console.log('Slide 2');
+    cleanCanvas('#slide2container');
+    d3.select('#slide2container')
+        .append("svg")
+        .attr("width", 500)
+        .attr("height", 500)
+        .append("circle")
+        .attr("cx", 100)
+        .attr("cy", 100)
+        .attr("r", 100)
+        .style("fill", "red");
+});
+
+Reveal.addEventListener('slide3', () => {
+    console.log('Slide 3');
+});
