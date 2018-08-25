7782b3fa89c16fdf1a341c9d13561444ce341826
Daniel Flórez <danielflorez111@hotmail.com>
2018-08-25 08:59:21 -0500

    second bubble chart

--- hurtos2016/js/functions.js
+++ hurtos2016/js/functions.js
@@ -41,7 +41,7 @@ function createBubleChart(data, element) {
 
     var nodes = d3.hierarchy(dataset)
         .sum(function (d) {
-            return d.Poblacion;
+            return d.Conteo;
         });
 
     var node = svg.selectAll(".node")
@@ -74,7 +74,7 @@ function createBubleChart(data, element) {
 
     node.selectAll('circle')
         .on("mouseover", function (d) {
-            tooltip.text(`Hurtos: ${d.data.Conteo}`);
+            tooltip.text(`${d.data.Departamento}: ${d.data.Conteo}`);
             return tooltip.style("visibility", "visible");
         })
         .on("mousemove", function () {
@@ -101,17 +101,107 @@ function createBubleChart(data, element) {
 }
 
 function createBubleChartIntensity(data, element) {
-    var svg = d3.select(".deptos");
-    var nodes = svg.selectAll(".node");
+    // var svg = d3.select(".deptos");
+    // var nodes = svg.selectAll(".node");
 
-    nodes.selectAll('circle')
+    // nodes.selectAll('circle')
+    //     .style("fill", function (d, i) {
+
+    //     });
+
+    cleanCanvas(element);
+    dataset = {
+        "children": data
+    };
+
+    var diameter = 600;
+    var color = d3.scaleOrdinal(d3.schemeCategory20c);
+
+    var bubble = d3.pack(dataset)
+        .size([diameter, diameter])
+        .padding(1.5);
+
+    var svg = d3.select(element)
+        .append("svg")
+        .attr("width", diameter)
+        .attr("height", diameter)
+        .attr("class", "bubble")
+        .attr("class", "intensity");
+
+    var tooltip = d3.select("body")
+        .append("div")
+        .style("position", "absolute")
+        .style("z-index", "10")
+        .style("visibility", "hidden")
+        .style("color", "white")
+        .style("padding", "8px")
+        .style("background-color", "rgba(0, 0, 0, 0.75)")
+        .style("border-radius", "6px")
+        .style("font", "14px sans-serif")
+
+    var nodes = d3.hierarchy(dataset)
+        .sum(function (d) {
+            return d.Poblacion;
+        });
+
+    var node = svg.selectAll(".node")
+        .data(bubble(nodes).descendants())
+        .enter()
+        .filter(function (d) {
+            return !d.children
+        })
+        .append("g")
+        .attr("class", "node")
+        .attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+
+    node.append("title")
+        .text(function (d) {
+            return d.Departamento;
+        });
+
+    node.append("circle")
+        .attr("r", function (d) {
+            return d.r;
+        })
         .style("fill", function (d, i) {
             if (d.data.Proporcion <= 0.25) {
-                return 'green';
+                return '#40CC5C';
             } else {
-                return 'red'
+                return '#F22400'
             }
+        })
+        .style('pointer-events', 'all')
+        .style('cursor', 'pointer');
+
+
+    node.selectAll('circle')
+        .on("mouseover", function (d) {
+            tooltip.text(`${d.data.Departamento}: ${d.data.Conteo}`);
+            return tooltip.style("visibility", "visible");
+        })
+        .on("mousemove", function () {
+            return tooltip.style("top", (event.pageY - 10) + "px").style("left", (event.pageX + 10) + "px");
+        })
+        .on("mouseout", function () {
+            return tooltip.style("visibility", "hidden");
         });
+
+    node.append("text")
+        .attr("dy", ".2em")
+        .style("text-anchor", "middle")
+        .text(function (d) {
+            return d.data.Departamento.substring(0, d.r / 3);
+        })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", function (d) {
+            return d.r / 5;
+        })
+        .attr("fill", "white");
+
+    d3.select(self.frameElement)
+        .style("height", diameter + "px");
 }
 
 function doStep(step) {
