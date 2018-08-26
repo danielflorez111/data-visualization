f3c6d6bf3dc2ff1f5339d6b5da576121c7c44436
Daniel Flórez <danielflorez111@hotmail.com>
2018-08-25 20:27:48 -0500

    Donut chart

--- hurtos2016/js/functions.js
+++ hurtos2016/js/functions.js
@@ -379,8 +379,46 @@ function createBarChart(data, element) {
         });
 }
 
-function createDonutChart(data, element){
-    
+function createDonutChart(data, element) {
+    cleanCanvas(element);
+
+    let diameter = 650,
+        svg = d3.select(element)
+            .append("svg")
+            .attr("width", diameter)
+            .attr("height", diameter),
+        width = +svg.attr("width"),
+        height = +svg.attr("height"),
+        radius = Math.min(width, height) / 2,
+        g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+    var pie = d3.pie()
+        .sort(null)
+        .value(function (d) { return d.Conteo; });
+
+    var path = d3.arc()
+        .outerRadius(radius - 10)
+        .innerRadius(radius - 70);
+
+    var label = d3.arc()
+        .outerRadius(radius - 40)
+        .innerRadius(radius - 40);
+
+    var arc = g.selectAll(".arc")
+        .data(pie(data))
+        .enter().append("g")
+        .attr("class", "arc");
+
+    arc.append("path")
+        .attr("d", path)
+        .attr("fill", function (d) { return color(d.data.Arma_empleada); });
+
+    arc.append("text")
+        .attr("transform", function (d) { return "translate(" + label.centroid(d) + ")"; })
+        .attr("dy", "0.1em")
+        .text(function (d) { return d.data.Arma_empleada; });
 }
 
 function doStep(step) {
